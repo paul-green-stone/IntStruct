4ffe9745deb7abff11bbf79f2af8ca98f90b524c
paulstone <stone.paul.green>
2025-12-24 01:25:20 +0300

    Add a function to retrieve a pointer to a byte

--- instruct.h
+++ instruct.h
@@ -14,7 +14,7 @@ extern "C" {
 /* ================================================================ */
 
 /**
- * 
+ * A utility that prints the raw memory contents at `mem_reg` (first `num_bytes` bytes) as a space-separated sequence of two-digit uppercase hexadecimal values.
  */
 static inline void log_as_bytes(const void* mem_reg, size_t num_bytes) {
 
@@ -29,7 +29,7 @@ static inline void log_as_bytes(const void* mem_reg, size_t num_bytes) {
 }
 
 /**
- * 
+ * A utility that prints the raw memory contents at `mem_reg` (first `num_bytes` bytes) as space-separated binary strings.
  */
 static inline void log_as_bits(const void* mem_reg, size_t num_bytes) {
 
@@ -47,7 +47,7 @@ static inline void log_as_bits(const void* mem_reg, size_t num_bytes) {
 }
 
 /**
- * 
+ * A utility that detects the system's endianness. It's either `L` (little-endian) or `B` (big-endian) 
  */
 static inline char get_endianness(void) {
 
@@ -57,41 +57,41 @@ static inline char get_endianness(void) {
 }
 
 /**
- * 
+ * An accessor that safely extracts a specific byte from memory at `mem_reg` within `num_bytes` bounds.
  */
-static inline unsigned char get_byte(const void* mem_reg, size_t num_bytes, int byte) {
-    return *(((unsigned char*) mem_reg) + ((byte < 1) ? 0 : (byte > num_bytes) ? num_bytes - 1 : byte - 1));
+static inline unsigned char get_byte(const void* mem_reg, size_t num_bytes, int byte_idx) {
+    return *(((unsigned char*) mem_reg) + ((byte_idx < 1) ? 0 : (byte_idx > num_bytes) ? num_bytes - 1 : byte_idx - 1));
 }
 
 /**
- * 
+ * A utility that safely sets a specific byte in memory at `mem_reg` within `num_bytes` bounds to the given value.
  */
-static inline void set_byte(void* mem_reg, size_t num_bytes, int byte, unsigned char value) {
-    *(((unsigned char*) mem_reg) + ((byte < 1) ? 0 : (byte > num_bytes) ? num_bytes - 1 : byte - 1)) = value;
+static inline void set_byte(void* mem_reg, size_t num_bytes, int byte_idx, unsigned char value) {
+    *(((unsigned char*) mem_reg) + ((byte_idx < 1) ? 0 : (byte_idx > num_bytes) ? num_bytes - 1 : byte_idx - 1)) = value;
 }
 
 /**
- * 
+ * A utility that sets a specific bit in the first byte of memory at `mem_reg` to `1`.
  */
-static inline void set_bit(void* mem_reg, size_t num_bytes, int bit) {
+static inline void set_bit(void* mem_reg, size_t num_bytes, int bit_idx) {
 
-    if (bit == 0) { return ; }
+    if (bit_idx == 0) { return ; }
 
-    *((unsigned char*) mem_reg) |= (1 << ((bit >= CHAR_BIT) ? 7 : bit - 1));
+    *((unsigned char*) mem_reg) |= (1 << ((bit_idx >= CHAR_BIT) ? 7 : bit_idx - 1));
 }
 
 /**
- * 
+ * A utility that sets a specific bit in the first byte of memory at `mem_reg` to `0`.
  */
-static inline void clear_bit(void* mem_reg, size_t num_bytes, int bit) {
+static inline void clear_bit(void* mem_reg, size_t num_bytes, int bit_idx) {
 
-    if (bit == 0) { return ; }
+    if (bit_idx == 0) { return ; }
 
-    *((unsigned char*) mem_reg) &= ~(1 << ((bit >= CHAR_BIT) ? 7 : bit - 1));
+    *((unsigned char*) mem_reg) &= ~(1 << ((bit_idx >= CHAR_BIT) ? 7 : bit_idx - 1));
 }
 
 /**
- * 
+ * This prints each byte in a memory region along with its address
  */
 static inline void log_as_bytesV(const void* mem_reg, size_t num_bytes) {
 
@@ -103,7 +103,7 @@ static inline void log_as_bytesV(const void* mem_reg, size_t num_bytes) {
 }
 
 /**
- * 
+ * This logs each byte of memory starting at `mem_reg` as a sequence of bits, printing the address and bit representation for each byte individually.
  */
 static inline void log_as_bitsV(const void* mem_reg, size_t num_bytes) {
 
@@ -112,6 +112,13 @@ static inline void log_as_bitsV(const void* mem_reg, size_t num_bytes) {
         printf("Value  : ");
         log_as_bits(mem_reg + i, sizeof(unsigned char));
     }
+}
+
+/**
+ * A utility that returns a pointer to the byte at the specified position in `mem_reg`.
+ */
+static inline unsigned char* ret_byte(const void* mem_reg, size_t num_bytes, int byte_idx) {
+    return (((unsigned char*) mem_reg) + ((byte_idx < 1) ? 0 : (byte_idx > num_bytes) ? num_bytes - 1 : byte_idx - 1));
 }
 
 /* ================================================================ */
